a9093166b4a3f76a765e3e722a55192fdad30fd5
Jose Almino <jose.almino.jr@gmail.com>
2024-12-24 11:25:16 -0300

    include modulo karpenter

--- main.tf
+++ main.tf
@@ -39,6 +39,11 @@ module "eks_blueprints" {
       username = "ops-role"
       groups   = ["system:masters"]
     },
+    # {
+    #   rolearn  = "arn:aws:iam::${local.account_id}:role/KarpenterIRSA-eks-lab"
+    #   username = "system:node:{{EC2PrivateDNSName}}"
+    #   groups   = ["system:bootstrappers", "system:nodes"]
+    # },
     {
       rolearn  = "arn:aws:iam::${local.account_id}:role/eks-admin"
       username = "eks-admin"
@@ -67,22 +72,6 @@ module "eks_blueprints" {
 
   # EKS MANAGED NODE GROUPS
   managed_node_groups = {
-    #    T3A_MICRO = {
-    #      node_group_name = local.node_group_name
-    #      instance_types  = ["t3a.micro"]
-    #      subnet_ids      = module.vpc.private_subnets
-    #      min_size     = 2
-    #      max_size     = 10
-    #      desired_size = 6
-    #    },
-    #    T3_MICRO = {
-    #      node_group_name = local.node_group_name_2
-    #      instance_types  = ["t3.micro"]
-    #      subnet_ids      = module.vpc.private_subnets
-    #      min_size     = 2
-    #      max_size     = 10
-    #      desired_size = 6
-    #    },
     T3_MEDIUM = {
       node_group_name = local.node_group_name
       instance_types  = ["t3.medium"]
@@ -106,8 +95,11 @@ module "eks_blueprints" {
   tags = local.tags
 }
 
-# VPC
+output "debug_managed_node_group_arn" {
+  value = module.eks_blueprints.managed_node_group_arn
+}
 
+# VPC
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.14.0"
@@ -135,6 +127,7 @@ module "vpc" {
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.name}" = "shared"
     "kubernetes.io/role/elb"              = "1"
+    "karpenter.sh/discovery" = null
   }
 
   private_subnet_tags = {
@@ -143,6 +136,117 @@ module "vpc" {
   }
 
   tags = local.tags
+}
+
+module "karpenter" {
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "19.20.0"
+
+  cluster_name                    = local.name
+  irsa_oidc_provider_arn          = module.eks_blueprints.eks_oidc_provider_arn
+  irsa_namespace_service_accounts = ["karpenter:karpenter"]
+
+  create_iam_role      = false
+  iam_role_arn         = "arn:aws:iam::058264204627:role/eks-lab-managed-ondemand"
+  irsa_use_name_prefix = false
+
+  tags = local.tags
+}
+
+provider "aws" {
+    region = "us-east-1"
+    alias  = "ecr"
+}
+
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.ecr
+}
+
+resource "helm_release" "karpenter" {
+  namespace        = "karpenter"
+  create_namespace = true
+
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter"
+  version             = "v0.31.3"
+
+  set {
+    name  = "settings.aws.clusterName"
+    value = local.name
+  }
+
+  set {
+    name  = "settings.aws.clusterEndpoint"
+    value = module.eks_blueprints.eks_cluster_endpoint
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.karpenter.irsa_arn
+  }
+
+  set {
+    name  = "settings.aws.defaultInstanceProfile"
+    value = module.karpenter.instance_profile_name
+  }
+
+  set {
+    name  = "settings.aws.interruptionQueueName"
+    value = module.karpenter.queue_name
+  }
+}
+
+resource "kubectl_manifest" "karpenter_provisioner" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.sh/v1alpha5
+    kind: Provisioner
+    metadata:
+      name: default
+    spec:
+      requirements:
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand"] #"spot"
+        - key: karpenter.k8s.aws/instance-category
+          operator: In
+          values: ["c", "m", "r"]
+        # - key: "node.kubernetes.io/instance-type"
+        #   operator: In
+        #   values: ["c5.large","c5a.large", "c5ad.large", "c5d.large", "c6i.large", "t2.medium", "t3.medium", "t3a.medium"]
+      limits:
+        resources:
+          cpu: 1000
+      providerRef:
+        name: default
+      ttlSecondsAfterEmpty: 30
+  YAML
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+resource "kubectl_manifest" "karpenter_node_template" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1alpha1
+    kind: AWSNodeTemplate
+    metadata:
+      name: default
+    spec:
+      subnetSelector:
+        karpenter.sh/discovery: ${local.name}
+      securityGroupSelector:
+        karpenter.sh/discovery: ${local.name}
+      tags:
+        karpenter.sh/discovery: ${local.name}
+  YAML
+
+  depends_on = [
+    helm_release.karpenter
+  ]
 }
 
 # Manifestos
